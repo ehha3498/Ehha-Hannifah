9d857ab283951ba7e02427a8ce2c0841296adc0a
ehha3498 <37094327+ehha3498@users.noreply.github.com>
2018-04-10 22:27:00 +0700

    Add files via upload

---
+++ Input for.py
@@ -0,0 +1,27 @@
+jumlah = 10
+for i in range(jumlah):
+        print("perulangan ke:", i)
+
+print("\n\n")
+A = ['a', 'b', 'c', 'd', 'e']
+print(A)
+for a in A:
+        print("nilai a=", a)
+        
+for b in range(10, 0, -1):
+        print("perulangan ke", b)
+        
+print "******************************"
+item = ['kopi','nasi','teh','jeruk']
+for isi in item:
+    print isi 
+
+print "******************************"
+import random
+
+while 1:
+        i = random.random()
+        print(i)
+        if i > 0.5:
+                 break
+print("selesai")
